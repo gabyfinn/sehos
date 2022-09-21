7004ea9b98eada071aa137da1533ab556230d53c
Gabyfinn <gabrieluzzante@gmail.com>
2022-09-21 13:16:31 -0300

    Fixing searchbar

--- src/controllers/SearchBar.ts
+++ src/controllers/SearchBar.ts
@@ -1,20 +1,27 @@
 /* import { Products, Users, Category, Product_details, Color, Images, Sizes } from "../db";
 import { Op } from "sequelize" */
-import axios from 'axios';
+
+/* import { where } from 'sequelize/types'; */
+
+import { Products } from '../db';
+import { ProductsI } from '../types';
 
 export const getProducts = async (product: string): Promise<any> => {
 	try {
 		if (!product) throw new Error(`there's no any product for` + product);
 
-		const { data }: any = await axios.get('http://localhost:3001/products');
+		/* const { data }: any = await axios.get('http://localhost:3001/products');
 
 		const filtrados: Array<any> = data.filter((p: any) =>
 			p.name.toLowerCase().includes(product.toLowerCase())
-		);
+		); */
+		const filtrados: any = await Products.findAll()
+		let exp = new RegExp(product.toLowerCase());
+		/* console.log("Entre al al title"); */
+		let result = filtrados?.filter((e: ProductsI) => exp.test(e.name.toLowerCase()));
+		if (!result) throw new Error(`Theres no coincidence for ${product}`);
 
-		if (!filtrados) throw new Error(`Theres no coincidence for ${product}`);
-
-		return filtrados;
+		return result;
 	} catch (error: any) {
 		return [];
 	}
